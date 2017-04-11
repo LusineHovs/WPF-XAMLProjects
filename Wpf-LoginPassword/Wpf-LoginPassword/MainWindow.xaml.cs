ac87bea49d7bc5679dd162a27f5355b965379d10
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-04-11 17:09:12 +0400

    Additions

---
+++ Wpf-LoginPassword/Wpf-LoginPassword/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_LoginPassword
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if ((Login.Text == string.Empty && Password.Password == string.Empty) || (Password.Password.Length <= 6 && Password.Password.Length >= 15))
+            {
+                MessageBox.Show("Please correctly enter the Login");
+            }
+            else
+            {
+                if (Stay.IsChecked == true)
+                {
+                    MessageBox.Show($"Hello {Login.Text}  \n your login and password have been saved!!! ");
+                }
+                else
+                {
+                    MessageBox.Show($"Hello {Login.Text} ");
+                }
+
+            }
+        }
+
+        private void Stay_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Stay.IsChecked == false)
+            {
+                MessageBox.Show("Do you want to keep your password?");
+            }
+        }
+    }
+}
