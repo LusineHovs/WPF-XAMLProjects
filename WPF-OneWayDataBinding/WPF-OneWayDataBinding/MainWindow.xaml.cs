6f819ec2d5dd3681f96c35c669abb627533c4e7f
Lusine Hovsepyan <lusine@hovsepyan.am>
2017-04-12 11:06:51 +0400

    Addition

---
+++ WPF-OneWayDataBinding/WPF-OneWayDataBinding/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_OneWayDataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Person person = new Person { Name = "Salman", Age = 26 };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string message = person.Name + " is " + person.Age;
+            MessageBox.Show(message);
+        }
+    }
+
+    public class Person
+    {
+
+        private string nameValue;
+
+        public string Name
+        {
+            get { return nameValue; }
+            set { nameValue = value; }
+        }
+
+        private double ageValue;
+
+        public double Age
+        {
+            get { return ageValue; }
+
+            set
+            {
+                if (value != ageValue)
+                {
+                    ageValue = value;
+                }
+            }
+        }
+    }
+}
